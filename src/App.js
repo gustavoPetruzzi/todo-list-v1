1227eef11c0e94d1f60b05bb7f925e93d943ead5
Gustavo Petruzzi <gustavopetruzzi@gmail.com>
2021-11-09 01:43:02 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const itemRef = useRef();
+  const onAddHandler = (event) => {
+    event.preventDefault();
+    setTodoList( (prevState) => ([...prevState, itemRef.current.value]))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centered list">
+      <h1> Todo List</h1>
+      <form className="centered list__form" onSubmit={onAddHandler}>
+        <label htmlFor="item"> Nuevo Item </label>
+        <input id="item" ref={itemRef} />
+        <button type="submit"> Agregar </button>
+      </form>
+      <ul className="centered list__items">
+        {todoList.map((item, index) => <li key={index}> {item} </li>)}
+      </ul>
     </div>
   );
 }
